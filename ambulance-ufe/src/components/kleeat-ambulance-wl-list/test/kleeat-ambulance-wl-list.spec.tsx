a2f74e14abef4ad37e8b3d31894ec5e4bc9c9602
Levente Szénási <91260091+Kleeat@users.noreply.github.com>
2026-02-17 14:54:42 +0100

    unit tests

--- ambulance-ufe/src/components/kleeat-ambulance-wl-list/test/kleeat-ambulance-wl-list.spec.tsx
+++ ambulance-ufe/src/components/kleeat-ambulance-wl-list/test/kleeat-ambulance-wl-list.spec.tsx
@@ -7,12 +7,10 @@ describe('kleeat-ambulance-wl-list', () => {
       components: [KleeatAmbulanceWlList],
       html: `<kleeat-ambulance-wl-list></kleeat-ambulance-wl-list>`,
     });
-    expect(page.root).toEqualHtml(`
-      <kleeat-ambulance-wl-list>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </kleeat-ambulance-wl-list>
-    `);
+    const wlList = page.rootInstance as KleeatAmbulanceWlList;
+      const expectedPatients = wlList?.waitingPatients?.length
+
+      const items = page.root.shadowRoot.querySelectorAll("md-list-item");
+      expect(items.length).toEqual(expectedPatients);
   });
 });
